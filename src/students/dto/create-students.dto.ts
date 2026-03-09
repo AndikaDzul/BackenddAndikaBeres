afbf27290af19d914026b4fc7e80e270c86aa557
AndikaDzul <dika70287@gmail.com>
2026-03-09 21:10:14 +0700

    Fix: Export bootstrap and update vercel entry point

--- src/students/dto/create-students.dto.ts
+++ src/students/dto/create-students.dto.ts
@@ -1,24 +1,7 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 export class CreateStudentDto {
-
-  @IsNotEmpty()
-  @IsString()
-  name: string
-
-  @IsNotEmpty()
-  @IsString()
-  nis: string
-
-  @IsOptional()
-  @IsString()
-  email?: string
-
-  @IsNotEmpty()
-  @IsString()
-  password: string
-
-  @IsOptional()
-  @IsString()
-  class?: string
+  nis: string;
+  name: string;
+  class: string;
+  password: string;
 }
